4d6139fb027acd0656b075cab503dc1ea961033a
zhibiao <243027820@qq.com>
2017-10-18 15:30:39 +0800

    Initialize the nodejs server.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const app = express();  
+app.use(express.static('dist'));
+
+const server = https.createServer( {
+	key: fs.readFileSync('ssl-key/key.pem'),
+	cert: fs.readFileSync('ssl-key/cert.pem')
+}, app);  
+
+server.listen(443, function(){
+	console.log('Server running at https://localhost:443/');
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function (socket) {
+	
+});
+
+
+
+
+
+
+
